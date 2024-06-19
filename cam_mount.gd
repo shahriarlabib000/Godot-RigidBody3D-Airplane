fa49b3f236c37499894998e59705e664fe8accea
shahriar <shahriarlabib000@gmail.com>
2024-06-19 14:37:11 +0600

    camera improvments

--- cam_mount.gd
+++ cam_mount.gd
@@ -1,6 +1,8 @@
 extends Node3D
+func _physics_process(delta: float) -> void:
+	$Camera3D.global_rotation.z=0
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventScreenDrag:
-		rotation.x += event.relative.y * 0.01
-		rotation.y -= event.relative.x * 0.01
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventScreenDrag:
+		#rotation.x += event.relative.y * 0.01
+		#rotation.y -= event.relative.x * 0.01
